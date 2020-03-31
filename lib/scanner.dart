4daeb2cfa7637d7079e4518ae26af4660f80d1bd
mike038 <miguel.aguilar038@gmail.com>
2020-03-31 10:07:08 -0600

    Update scanner.dart
    Agregar cámara de dispositivo y cambiar estados en pantalla Escaner

--- lib/scanner.dart
+++ lib/scanner.dart
@@ -1,27 +1,91 @@
 import 'package:flutter/material.dart';
- 
-void main() => runApp(Scanner());
- 
-class Scanner extends StatelessWidget {
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+
+class EscanerPage extends StatefulWidget {
+  @override
+  _EscanerPageState createState() => _EscanerPageState();
+}
+
+class _EscanerPageState extends State<EscanerPage> {
+  File _selectedFile;
+  bool _inProcess = false;
+
+  Widget getImageWidget() {
+    if (_selectedFile != null) {
+      return Image.file(
+        _selectedFile,
+        width: 250,
+        height: 350,
+        fit: BoxFit.cover,
+      );
+    } else {
+      return Image.asset(
+        "assets/placeholder.jpg",
+        width: 250,
+        height: 250,
+        fit: BoxFit.cover,
+      );
+    }
+  }
+
+  getImage(ImageSource source) async {
+    this.setState(() {
+      _inProcess = true;
+    });
+    File image = await ImagePicker.pickImage(source: source);
+    this.setState(() {
+      _selectedFile = image;
+      _inProcess = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
-          title: Text("Escaner"),
+          title: Text('Escaner'),
         ),
-        body: Center(
-          child: RaisedButton(
-            onPressed: () {Navigator.of(context).push(
-              MaterialPageRoute(builder: (_) => Camara()
+        body: Stack(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  getImageWidget(),
+                  MaterialButton(
+                    color: Colors.blue,
+                    child: Text(
+                      "Tomar foto",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {
+                      getImage(ImageSource.camera);
+                    },
+                  ),
+                  MaterialButton(
+                      color: Colors.blue,
+                      child: Text(
+                        'Escanear',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {}),
+                ],
               ),
-              );
-              },
-            child: Text("Escanear"),
-          ),
+            ),
+            (_inProcess)
+                ? Container(
+                    color: Colors.white,
+                    height: MediaQuery.of(context).size.height * 0.95,
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  )
+                : Center()
+          ],
         ),
-      ),
     );
   }
 }
